67311c266adc111547ceaea5617d48582fd1afc8
megaqwertification <megaqwertification@gmail.com>
2022-05-22 19:49:37 -0400

    Initial git commit

---
+++ constants.py
@@ -0,0 +1,77 @@
+CHARACTERS = [
+    "Dr. Mario",
+    "Mario",
+    "Luigi",
+    "Bowser",
+    "Peach",
+    "Yoshi",
+    "DK",
+    "Falcon",
+    "Ganondorf",
+    "Falco",
+    "Fox",
+    "Ness",
+    "Popo",
+    "Kirby",
+    "Samus",
+    "Sheik",
+    "Link",
+    "Young Link",
+    "Pichu",
+    "Pikachu",
+    "Jigglypuff",
+    "Mewtwo",
+    "Mr. G&W",
+    "Marth",
+    "Roy",
+    "Zelda",
+    "Ice Climbers",
+    "M. Wireframe",
+    "F. Wireframe",
+    "Giga Bowser",
+    "Master Hand",
+    "Crazy Hand",
+]
+
+AKANEIA_CHARACTERS = [
+    "Wolf",
+    "Diddy Kong",
+    "Charizard"
+]
+# TODO: add akaneia versions
+
+STAGES = [
+    "Dr. Mario",
+    "Mario",
+    "Luigi",
+    "Bowser",
+    "Peach",
+    "Yoshi",
+    "DK",
+    "Falcon",
+    "Ganondorf",
+    "Falco",
+    "Fox",
+    "Ness",
+    "Ice Climbers",
+    "Kirby",
+    "Samus",
+    "Sheik",
+    "Link",
+    "Young Link",
+    "Pichu",
+    "Pikachu",
+    "Jigglypuff",
+    "Mewtwo",
+    "Mr. G&W",
+    "Marth",
+    "Roy",
+    "Seak",
+]
+
+VERSIONS = [
+    "NTSC 1.00",
+    "NTSC 1.01",
+    "NTSC 1.02",
+    "PAL"
+]
---
+++ functions.py
@@ -0,0 +1,20 @@
+add_record
+    # check if exists
+
+delete_record
+
+
+
+insert_source
+
+
+
+
+
+view_record
+
+
+view_history
+
+
+
