aee2bba423d146c7f52e0b7724888b2f08cc07dd
megaqwertification <megaqwertification@gmail.com>
2022-08-05 20:51:23 -0400

    Disable hrc history command until database is filled

--- commands/hrc_commands.py
+++ commands/hrc_commands.py
@@ -140,137 +140,137 @@ def register_hrc_commands(bot: Client):
         cur.close()
         conn.close()
 
-    @bot.command(
-        name='hrc-history',
-        description='Display WR history for HRC character', # should i add WR ties lol. if possible? who knows at this point
-        scope=[PERSONAL_GUILD_ID],
-        options=[
-            # TODO: evalute if choices can be from list
-            Option(
-                name='character',
-                description='Choose your character',
-                type=OptionType.STRING,
-                required=True,
-                #choices = [Choice(name="character", value=char ) for char in HRC_CHARACTERS]
-            ),
-            Option(
-                name='tas',
-                description='RTA vs. TAS (default RTA)', # TODO: standardize descriptions of parameters
-                type=OptionType.BOOLEAN,
-                required=False,
-            )
-        ],
-    )
+    # @bot.command(
+    #     name='hrc-history',
+    #     description='Display WR history for HRC character', # should i add WR ties lol. if possible? who knows at this point
+    #     scope=[PERSONAL_GUILD_ID],
+    #     options=[
+    #         # TODO: evalute if choices can be from list
+    #         Option(
+    #             name='character',
+    #             description='Choose your character',
+    #             type=OptionType.STRING,
+    #             required=True,
+    #             #choices = [Choice(name="character", value=char ) for char in HRC_CHARACTERS]
+    #         ),
+    #         Option(
+    #             name='tas',
+    #             description='RTA vs. TAS (default RTA)', # TODO: standardize descriptions of parameters
+    #             type=OptionType.BOOLEAN,
+    #             required=False,
+    #         )
+    #     ],
+    # )
 
-    async def _hrc_history(ctx: CommandContext, **kwargs):
-        # TODO: add TAS flag (switch case which just affects the query "AND tas=false")
-        # TODO: experiment with a date argument so that you can get the history of the record at a certain date
-        # TODO: raise value errors
-        char_name = kwargs.get("character")
-        is_TAS = kwargs.get('tas', False)
-        conn = connect()
+    # async def _hrc_history(ctx: CommandContext, **kwargs):
+    #     # TODO: add TAS flag (switch case which just affects the query "AND tas=false")
+    #     # TODO: experiment with a date argument so that you can get the history of the record at a certain date
+    #     # TODO: raise value errors
+    #     char_name = kwargs.get("character")
+    #     is_TAS = kwargs.get('tas', False)
+    #     conn = connect()
 
-        # option 1: a master query that simply filters out for the best times per char
-        # option 2: loop through all 25 chars to get the wrs
-        # option 3: store all WRs in a separate table
-        # remember to think about ties, except ganon/ICs
+    #     # option 1: a master query that simply filters out for the best times per char
+    #     # option 2: loop through all 25 chars to get the wrs
+    #     # option 3: store all WRs in a separate table
+    #     # remember to think about ties, except ganon/ICs
 
-        sql_q = f'SELECT * FROM hrc_table WHERE character=\'{char_name}\' and tas={is_TAS} ORDER BY date ASC;'
-        cur = conn.cursor()
-        cur.execute(sql_q) # make an f-string that gets the best score for each char, what about WR ties? can
-        # i hard code "many" for ICs and Ganon? and just link to marth1 / joe bushman's vids?
-        # TODO: get this working for each character
-        # TODO: get this working so you dont have to grammatically be correct for each char
-        # TODO: add error exception if someone enters an unknown character
-        # TODO: make sure ties are not included in this, 
-        # TODO: decide if you're going to include WR ties with links
-        # TODO: add dates to front? or elsewhere? if so do i do it for each tie? would get preeeetty hectic but its the point of a history command
-        # TODO: make this into a table format of some sort
+    #     sql_q = f'SELECT * FROM hrc_table WHERE character=\'{char_name}\' and tas={is_TAS} ORDER BY date ASC;'
+    #     cur = conn.cursor()
+    #     cur.execute(sql_q) # make an f-string that gets the best score for each char, what about WR ties? can
+    #     # i hard code "many" for ICs and Ganon? and just link to marth1 / joe bushman's vids?
+    #     # TODO: get this working for each character
+    #     # TODO: get this working so you dont have to grammatically be correct for each char
+    #     # TODO: add error exception if someone enters an unknown character
+    #     # TODO: make sure ties are not included in this, 
+    #     # TODO: decide if you're going to include WR ties with links
+    #     # TODO: add dates to front? or elsewhere? if so do i do it for each tie? would get preeeetty hectic but its the point of a history command
+    #     # TODO: make this into a table format of some sort
 
-        description_lines = []
+    #     description_lines = []
 
-        prev_score = 0
+    #     prev_score = 0
 
-        for record in cur:
-            details = {
-                # possible to index by sql column name instead of integer?
-                "char": record[0],
-                "player" : record[1],
-                "score_ft" : record[2],
-                "score_m" : record[3],
-                "sources" : record[4],
-                "date" : record[5].date(),
-        #         "tas" : record[6],
-        #         "emulator" : record[7],
-        #         "tags" : record[8],
-        #         "version" : record[9]
-            }
-            #print(details['sources'])
-            char = details["char"]
-            score_ft = details["score_ft"]
-            score_m = details["score_m"]
-            player = details["player"]
-            date = details["date"]
+    #     for record in cur:
+    #         details = {
+    #             # possible to index by sql column name instead of integer?
+    #             "char": record[0],
+    #             "player" : record[1],
+    #             "score_ft" : record[2],
+    #             "score_m" : record[3],
+    #             "sources" : record[4],
+    #             "date" : record[5].date(),
+    #     #         "tas" : record[6],
+    #     #         "emulator" : record[7],
+    #     #         "tags" : record[8],
+    #     #         "version" : record[9]
+    #         }
+    #         #print(details['sources'])
+    #         char = details["char"]
+    #         score_ft = details["score_ft"]
+    #         score_m = details["score_m"]
+    #         player = details["player"]
+    #         date = details["date"]
 
-            if len(details['sources']) != 0: # should just make this "not empty" or something lol
-                video = details["sources"][0]
-            else:
-                video = None
-            tied_player = None
-            #     metre_sum += float(score_m)
+    #         if len(details['sources']) != 0: # should just make this "not empty" or something lol
+    #             video = details["sources"][0]
+    #         else:
+    #             video = None
+    #         tied_player = None
+    #         #     metre_sum += float(score_m)
 
-            #     description_lines.append(
-            #         f"{char} - \t [{score_ft}ft/{score_m}m]({video}) - \t {player}"
+    #         #     description_lines.append(
+    #         #         f"{char} - \t [{score_ft}ft/{score_m}m]({video}) - \t {player}"
 
 
-            # running max
-            #prev_score = score_ft
-            #print(description_lines[-1])
-            if score_ft <= prev_score: # use '<' if including ties
-                #prev_score = score_ft
-                continue
-            # REMOVE TIES
-            #elif score_ft == prev_score:
-            #    # encountered a tie, append player name
-            #    tied_player = record[1]
+    #         # running max
+    #         #prev_score = score_ft
+    #         #print(description_lines[-1])
+    #         if score_ft <= prev_score: # use '<' if including ties
+    #             #prev_score = score_ft
+    #             continue
+    #         # REMOVE TIES
+    #         #elif score_ft == prev_score:
+    #         #    # encountered a tie, append player name
+    #         #    tied_player = record[1]
 
-            prev_score = score_ft
-            if video == None:
-                description_lines.append(
-                    f'({date}) - {score_ft}ft/{score_m}m - {player}'
-                )    
-            else:
-                description_lines.append(
-                    f'({date}) - [{score_ft}ft/{score_m}m]({video}) - {player}'
-                )
+    #         prev_score = score_ft
+    #         if video == None:
+    #             description_lines.append(
+    #                 f'({date}) - {score_ft}ft/{score_m}m - {player}'
+    #             )    
+    #         else:
+    #             description_lines.append(
+    #                 f'({date}) - [{score_ft}ft/{score_m}m]({video}) - {player}'
+    #             )
 
-            #if tied_player != None:
-            #    description_lines.pop()
-            #    description_lines[-1] += f', {tied_player}'
-            #    # SCUFFED. I AM A SCUFFED PROGRAMMER
-            #    #description_lines[-1] = description_lines[0:-13] + f', {tied_player}' + description_lines[-13::]
-            #tied_player = None
+    #         #if tied_player != None:
+    #         #    description_lines.pop()
+    #         #    description_lines[-1] += f', {tied_player}'
+    #         #    # SCUFFED. I AM A SCUFFED PROGRAMMER
+    #         #    #description_lines[-1] = description_lines[0:-13] + f', {tied_player}' + description_lines[-13::]
+    #         #tied_player = None
         
-        #     )
+    #     #     )
 
-        description_lines.append(f'{"(TAS) " if is_TAS else ""}History of {char_name} HRC WRs (ft/m) (YYYY/MM/DD)\n')
-        # reverse list
-        description_lines.reverse()
+    #     description_lines.append(f'{"(TAS) " if is_TAS else ""}History of {char_name} HRC WRs (ft/m) (YYYY/MM/DD)\n')
+    #     # reverse list
+    #     description_lines.reverse()
 
-        # add to front of list
-        #description_lines.insert(0, f"History of {char_name} HRC WRs (ft/m) (YYYY/MM/DD)\n")
+    #     # add to front of list
+    #     #description_lines.insert(0, f"History of {char_name} HRC WRs (ft/m) (YYYY/MM/DD)\n")
 
-        #print(description_lines)
+    #     #print(description_lines)
         
-        # TODO: add links to history sheet? Only applicable for RTA for HRC
-        if not is_TAS:
-            description_lines.append(f'\n [Full HRC RTA History Sheet](https://docs.google.com/spreadsheets/d/1qbd8nquan3mGl87Ja1ogwK1SQIE_Fa-Yki-8wRbm9BY/edit#gid=0)')
+    #     # TODO: add links to history sheet? Only applicable for RTA for HRC
+    #     if not is_TAS:
+    #         description_lines.append(f'\n [Full HRC RTA History Sheet](https://docs.google.com/spreadsheets/d/1qbd8nquan3mGl87Ja1ogwK1SQIE_Fa-Yki-8wRbm9BY/edit#gid=0)')
 
-        await embeds.send_embeds(description_lines, ctx)
+    #     await embeds.send_embeds(description_lines, ctx)
         
 
-        cur.close()
-        conn.close()
+    #     cur.close()
+    #     conn.close()
        
 
     
