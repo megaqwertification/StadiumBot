c55ca902b066e74f95029e969a9cf58e6ce0f9cb
megaqwertification <megaqwertification@gmail.com>
2022-12-28 14:44:26 -0500

    Enable btt history command because vanilla runs all added

--- commands/btt_commands.py
+++ commands/btt_commands.py
@@ -4,7 +4,7 @@ import embeds
 from typing import List
 from interactions import CommandContext, Option, OptionType, Choice
 
-from constants import ALIASES, BTT_SUS_TAGS, BTT_STAGES, BTT_CHARACTERS, HRC_CHARACTERS, PERSONAL_GUILD_ID, STADIUM_GUILD_ID
+from constants import ALIASES, BTT_SUS_TAGS, BTT_STAGES, BTT_CHARACTERS, HRC_CHARACTERS, GUILD_IDS
 from formulas import get_char_name, time_to_frames, frames_to_time_string
 
 from db import connect
@@ -13,7 +13,7 @@ def register_btt_commands(bot: Client):
     @bot.command(
         name='btt-wr',
         description='Query a BtT WR',
-        scope=[PERSONAL_GUILD_ID, STADIUM_GUILD_ID],
+        scope=GUILD_IDS,
         options=[
             Option(
                 name='character',
@@ -140,7 +140,7 @@ def register_btt_commands(bot: Client):
     @bot.command(
         name='btt-wr-list',
         description='Display the list of current BtT WRs',
-        scope=[PERSONAL_GUILD_ID, STADIUM_GUILD_ID],
+        scope=GUILD_IDS,
         options=[
             # TODO: add parameters in case you want a char or stage total
             Option(
@@ -222,138 +222,164 @@ def register_btt_commands(bot: Client):
         conn.close()
 
 
-    # @bot.command(
-    #     name='btt-wr-history',
-    #     description='Display the history of a BtT pairing (includes ties)',
-    #     scope=[PERSONAL_GUILD_ID, STADIUM_GUILD_ID],
-    #     options=[
-    #         Option(
-    #             name='character',
-    #             description='Choose your character',
-    #             type=OptionType.STRING,
-    #             required=True,
-    #         ),
-    #         Option(
-    #             name='stage',
-    #             description='stage (leave blank for vanilla)',
-    #             type=OptionType.STRING,
-    #             required=False
-    #         ),
-    #         Option(
-    #             name='tas',
-    #             description='default: RTA',
-    #             type=OptionType.BOOLEAN,
-    #             required=False,
-    #         ),
-    #         # Option(
-    #         #     name='tags',
-    #         #     description='SuS tags (comma separated, case sensitive)',
-    #         #     type=OptionType.STRING,
-    #         #     required=False
-    #         # )
-    #     ]   
-    # )        
-    # async def _btt_wr_history(ctx: CommandContext, **kwargs):
-    #     char_input = kwargs.get("character")
-    #     char_name = get_char_name(char_input, ALIASES)
-    #     if char_name not in BTT_CHARACTERS:
-    #         description = f'Please select a valid character'
-    #         await ctx.send(description, ephemeral=True)
-    #         return None
+    @bot.command(
+        name='btt-wr-history',
+        description='Display the history of a BtT pairing (includes ties)',
+        scope=GUILD_IDS,
+        options=[
+            Option(
+                name='character',
+                description='Choose your character',
+                type=OptionType.STRING,
+                required=True,
+            ),
+            Option(
+                name='stage',
+                description='stage (leave blank for vanilla)',
+                type=OptionType.STRING,
+                required=False
+            ),
+            Option(
+                name='tas',
+                description='default: RTA',
+                type=OptionType.BOOLEAN,
+                required=False,
+            ),
+            Option(
+                name='tags',
+                description='SuS tags (comma separated, case sensitive)',
+                type=OptionType.STRING,
+                required=False
+            )
+        ]   
+    )  
+          
+    async def _btt_wr_history(ctx: CommandContext, **kwargs):
+        char_input = kwargs.get("character")
+        char_name = get_char_name(char_input, ALIASES)
+        if char_name not in BTT_CHARACTERS:
+            description = f'Please select a valid character'
+            await ctx.send(description, ephemeral=True)
+            return None
 
-    #     stage_input = kwargs.get('stage', char_name)
-    #     stage_name = get_char_name(stage_input, ALIASES)
-    #     if stage_name not in BTT_STAGES:
-    #         description = f'Please select a valid stage'
-    #         await ctx.send(description, ephemeral=True)
-    #         return None
+        stage_input = kwargs.get('stage', char_name)
+        stage_name = get_char_name(stage_input, ALIASES)
+        if stage_name not in BTT_STAGES:
+            description = f'Please select a valid stage'
+            await ctx.send(description, ephemeral=True)
+            return None
 
-    #     # catch all if "ICs" is only inputted for vanilla
-    #     if kwargs.get('stage') == None and char_name == 'Ice Climbers':
-    #         char_name = 'Popo'
-    #         stage_name = 'Ice Climbers'
+        # catch all if "ICs" is only inputted for vanilla
+        if kwargs.get('stage') == None and char_name == 'Ice Climbers':
+            char_name = 'Popo'
+            stage_name = 'Ice Climbers'
         
-    #     # temp catch for sheik/zelda stage name
-    #     if stage_name == 'Sheik':
-    #         stage_name = 'Zelda'
-    #         original_stage_name = 'Sheik'
+        # temp catch for sheik/zelda stage name
+        if stage_name == 'Sheik':
+            stage_name = 'Zelda'
+            original_stage_name = 'Sheik'
 
-    #     # Catch all if "Sopo" is only inputted for vanilla
-    #     if stage_name == 'Popo':
-    #         stage_name = 'Ice Climbers'
+        # Catch all if "Sopo" is only inputted for vanilla
+        if stage_name == 'Popo':
+            stage_name = 'Ice Climbers'
 
-    #     is_TAS = kwargs.get('tas', False)
+        is_TAS = kwargs.get('tas', False)
 
-    #     sus_tags = kwargs.get('tags', [])
-    #     tags_list = sus_tags.split(',') if sus_tags else []
-    #     if not set(tags_list).issubset(set(BTT_SUS_TAGS.keys())):
-    #         raise ValueError('One or more sus tags DNE')
+        sus_tags = kwargs.get('tags', [])
+        tags_list = sus_tags.split(',') if sus_tags else []
+        if not set(tags_list).issubset(set(BTT_SUS_TAGS.keys())):
+            description = f'Please select a valid SuS tag (case sensitive for now)'
+            await ctx.send(description, ephemeral=True)
+            return None
 
-    #     conn = connect()
-    #     sql_q = f'SELECT * FROM btt_table WHERE character=\'{char_name}\' AND stage=\'{stage_name}\' AND tas={is_TAS} ORDER BY date ASC;'
-    #     cur = conn.cursor()
-    #     cur.execute(sql_q)
+        conn = connect()
+        sql_q = f'SELECT * FROM btt_table WHERE character=\'{char_name}\' AND stage=\'{stage_name}\' AND tas={is_TAS} ORDER BY date ASC, score DESC;' #score DESC, date ASC;'
+        cur = conn.cursor()
+        cur.execute(sql_q)
 
-    #     if cur.rowcount == 0:
-    #         description='Sorry, DB not filled out yet or record DNE. Please ping mega if you think there is an error'
-    #         await ctx.send(description, ephemeral=True)
-    #         return None
+        if cur.rowcount == 0:
+            description='Sorry, DB not filled out yet or record DNE. Please ping mega if you think there is an error'
+            await ctx.send(description, ephemeral=True)
+            return None
 
+        # pre-processing for sus tags
+        if tags_list:
+            cur = [record for record in cur if set(tags_list).issubset(record[9])]
+        # filter out 1T tag or sub-10 target tags
+        # Temp if conditions, need to make it better
+        if '1T' not in tags_list:
+            cur = [record for record in cur if not set(['1T']).issubset(record[9])]
+        if 'misfire' not in tags_list and char_name == 'Luigi': # or 'misfire' not in tags_list or 'AR' not in tags_list:
+            cur = [record for record in cur if not set(['misfire']).issubset(record[9])]
+        if 'AR' not in tags_list and not is_TAS:
+            cur = [record for record in cur if not set(['AR']).issubset(record[9])]
+        # more temporary filtering, probably a more efficient way to do things
+        if 'LSS' not in tags_list:
+            cur = [record for record in cur if not set(['LSS']).issubset(record[9])]
+        if 'BSS' not in tags_list:
+            cur = [record for record in cur if not set(['BSS']).issubset(record[9])]
+        if 'RSS' not in tags_list:
+            cur = [record for record in cur if not set(['RSS']).issubset(record[9])]
+        if 'TSS' not in tags_list:
+            cur = [record for record in cur if not set(['TSS']).issubset(record[9])]
 
-    #     description_lines = []
-    #     prev_score = 999
+        description_lines = []
+        prev_score = 999
 
-    #     for record in cur:
-    #         sources = record[4]
-    #         if len(sources) != 0:
-    #             video = sources[0] 
-    #         else:
-    #             video = None
-    #         tied_player = None
-    #         player = record[2]
-    #         score = record[3]
-    #         if score > prev_score:
-    #             continue
-    #         prev_score = score
-    #         # TODO: compare date and add to history if record was beaten in the same day AND does not have timestamp
-    #         # maybe can check YT source of video
-    #         date = record[5].date()
-    #         if video == None:
-    #             description_lines.append(
-    #                 f'({date}) - {score} - {player}'
-    #             )
-    #         else:
-    #             description_lines.append(
-    #                 f'({date}) - [{score}]({video}) - {player}'
-    #             )
-    #         # have to think about ties with Debug vs non debug, since they'll be same # of frames but diff displayed time
+        for record in cur:
+            sources = record[4]
+            if len(sources) != 0:
+                video = sources[0] 
+            else:
+                video = None
+            tied_player = None
+            player = record[2]
+            score = record[3]
+            if score >= prev_score:
+                continue
+            prev_score = score
+            # TODO: compare date and add to history if record was beaten in the same day AND does not have timestamp
+            # maybe can check YT source of video
+            date = record[5].date()
+            if video == None:
+                description_lines.append(
+                    f'({date}) - {score} - {player}'
+                )
+            else:
+                description_lines.append(
+                    f'({date}) - [{score}]({video}) - {player}'
+                )
+            # have to think about ties with Debug vs non debug, since they'll be same # of frames but diff displayed time
             
-    #         # i do not think this does anything
-    #         if tied_player != None:
-    #            description_lines.pop()
-    #            description_lines[-1] += f', {tied_player}'
-    #            # SCUFFED. I AM A SCUFFED PROGRAMMER :happysquare:
-    #            #description_lines[-1] = description_lines[0:-13] + f', {tied_player}' + description_lines[-13::]
-    #         tied_player = None
+            # i do not think this does anything
+            if tied_player != None:
+               description_lines.pop()
+               description_lines[-1] += f', {tied_player}'
+               # SCUFFED. I AM A SCUFFED PROGRAMMER :happysquare:
+               #description_lines[-1] = description_lines[0:-13] + f', {tied_player}' + description_lines[-13::]
+            tied_player = None
         
-    #     #     )
-
-    #     description_lines.append(f'{"(TAS) " if is_TAS else ""}History of {char_name}/{stage_name} BTT WRs (YYYY-MM-DD)\n')
-    #     # reverse list
-    #     description_lines.reverse()
-
-    #     # add to front of list
-    #     #description_lines.insert(0, f"History of {char_name} HRC WRs (ft/m) (YYYY/MM/DD)\n")
-
-    #     #print(description_lines)
+        #     )
+        if tags_list:
+            tags_str = ",".join(tags_list)
+            description_lines.append(f'SuS Tags: {tags_str}\n')
+        description_lines.append(f'{"(TAS) " if is_TAS else ""}History of {char_name}/{stage_name} BTT WRs (YYYY-MM-DD)\n')
         
-    #     # TODO: handle vanilla and mm history sheet appending
-    #     #if char_name:
-    #     #   description_lines.append(f'\n [Full BTT RTA History Sheet](https://docs.google.com/spreadsheets/d/1oenxMdKsnD9uppK01fPtp4dD6jpqYMv4w2DKXI6plqE/edit#gid=0)')
+        # reverse list
+        description_lines.reverse()
+
+        # add to front of list
+        #description_lines.insert(0, f"History of {char_name} HRC WRs (ft/m) (YYYY/MM/DD)\n")
+
+        #print(description_lines)
+        
+        # TODO: handle vanilla and mm history sheet appending
+        #if char_name:
+        #   description_lines.append(f'\n [Full BTT RTA History Sheet](https://docs.google.com/spreadsheets/d/1oenxMdKsnD9uppK01fPtp4dD6jpqYMv4w2DKXI6plqE/edit#gid=0)')
         
 
-    #     await embeds.send_embeds(description_lines, ctx)
+        await embeds.send_embeds(description_lines, ctx)
         
 
-    #     cur.close()
-    #     conn.close()
+        #cur.close() # not necessary if we're changing cur from cur to list
+        conn.close()
