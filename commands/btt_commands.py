c71f3a79076cad51fbd4532c7f914ec194674368
megaqwertification <megaqwertification@gmail.com>
2023-01-03 12:31:03 -0500

    Replace mm_TAS sheet link

--- commands/btt_commands.py
+++ commands/btt_commands.py
@@ -260,7 +260,7 @@ def register_btt_commands(bot: Client):
         vanilla_RTA_playlist = "https://www.youtube.com/playlist?list=PLZmpvgqEAI7DioaEmIJG832_XKVpJHhy_"
         vanilla_TAS_playlist = "https://www.youtube.com/playlist?list=PLP-fO_NfCBaqcicPbCvGQOCIN1pL9hZyA"
         mismatch_RTA_sheet = "https://docs.google.com/spreadsheets/d/1e8FNXsXG-y_ylUbWzA0S6ni1Yl4Sj73mINGneBSG164/edit#gid=0"
-        mismatch_TAS_sheet = "https://docs.google.com/spreadsheets/d/1BZEbvzlvWQSoUN1a_tcgxeBYmjLgla-m_5BZwinUUd0/edit#gid=335865100"
+        mismatch_TAS_sheet = "https://docs.google.com/spreadsheets/d/1Nr1PDHO3UeH3oQSeEjeFNdEKrfEdsc9GxbsEWvEuFuo/edit#gid=1672544190"
         
         if not is_TAS and char_input == None:
             hyperlink = vanilla_RTA_playlist
