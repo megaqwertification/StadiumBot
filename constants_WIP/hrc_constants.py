d14cdcbf549f8c3a9bd66984cb5fc68042c7f9a5
megaqwertification <megaqwertification@gmail.com>
2023-01-27 22:25:25 -0500

    Refactoring changes, preliminary

---
+++ constants_WIP/hrc_constants.py
@@ -0,0 +1,9 @@
+HRC_SUS_TAGS = {
+    'NBA' : 'No Bat Allowed',
+    'NBD' : 'No Bat Drops',
+    'BMO' : 'B Moves Only',
+    'NJA' : 'No Jump Allowed',
+    'LTA' : 'Limited Time Attack',
+    'Falcon Bat Swing' : 'Use Falcon Bat Swing to hit Sandbag at end',
+    'AAA' : 'All Aerial Attacks',
+}
