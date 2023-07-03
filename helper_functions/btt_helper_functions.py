affc5bb95c112e9be54267e9f87e6fcc636cf0b1
megaqwertification <megaqwertification@gmail.com>
2023-07-03 16:23:49 -0400

    Refactor SQL query

--- helper_functions/btt_helper_functions.py
+++ helper_functions/btt_helper_functions.py
@@ -14,23 +14,23 @@ def filter_btt_tags(tags_list: list, cur) -> list:
     filtered_list = list(cur)
     
     if tags_list:
-        filtered_list = [record for record in filtered_list if set(tags_list).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if set(tags_list).issubset(record[3])]
 
     if '1T' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['1T']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['1T']).issubset(record[3])]
     if 'misfire' not in tags_list: # or 'misfire' not in tags_list or 'AR' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['misfire']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['misfire']).issubset(record[3])]
     if 'AR' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['AR']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['AR']).issubset(record[3])]
     # more temporary filtering, probably a more efficient way to do things
     if 'LSS' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['LSS']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['LSS']).issubset(record[3])]
     if 'BSS' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['BSS']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['BSS']).issubset(record[3])]
     if 'RSS' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['RSS']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['RSS']).issubset(record[3])]
     if 'TSS' not in tags_list:
-        filtered_list = [record for record in filtered_list if not set(['TSS']).issubset(record[9])]
+        filtered_list = [record for record in filtered_list if not set(['TSS']).issubset(record[3])]
 
     return filtered_list
 
@@ -40,7 +40,7 @@ def get_current_btt_wr(character, stage, is_TAS, tags_list):
 
 
     conn = connect()
-    sql_q = f'SELECT * FROM btt_table WHERE character=\'{character}\' AND stage=\'{stage}\' AND tas={is_TAS} ORDER BY score ASC, date ASC;'
+    sql_q = f'SELECT player, score, sources, tags FROM btt_table WHERE character=\'{character}\' AND stage=\'{stage}\' AND tas={is_TAS} ORDER BY score ASC, date ASC;'
     cur = conn.cursor()
     cur.execute(sql_q)
     
@@ -57,13 +57,13 @@ def get_current_btt_wr(character, stage, is_TAS, tags_list):
     curr_score = 999
     video = None
     for record in cur:
-        if record[3] > curr_score:
+        if record[1] > curr_score:
             break
-        players.append(record[2])
-        if record[3] == curr_score:
+        players.append(record[0])
+        if record[1] == curr_score:
             continue
-        score = record[3]
-        video = record[4] if video == None else video # what if no video for any record?
+        score = record[1]
+        video = record[2] if video == None else video # what if no video for any record?
 
         curr_score = score
         current_btt_wr = record
@@ -91,7 +91,7 @@ def get_char_total(char_name, is_TAS):
 
     for stage in BTT_STAGES[:-1]:
         cur = conn.cursor()
-        sql_q = f'SELECT * FROM btt_table WHERE character=\'{char_name}\' AND stage=\'{stage}\' AND tas={is_TAS} ORDER BY score ASC, date ASC;'
+        sql_q = f'SELECT player, score, sources, tags FROM btt_table WHERE character=\'{char_name}\' AND stage=\'{stage}\' AND tas={is_TAS} ORDER BY score ASC, date ASC;'
         # if stage_name == 'Seak' and char_input == None:
         #     continue
         cur.execute(sql_q)
@@ -112,13 +112,13 @@ def get_char_total(char_name, is_TAS):
             elif record[3] == curr_score:
                 players.append(record[2])
                 continue
-            char = record[0]
-            stage = record[1]
-            players.append(record[2])
-            score_time = record[3]
+            #char = record[0]
+            #stage = record[1]
+            players.append(record[0])
+            score_time = record[1]
             #print(stage)
-            if len(record[4]) != 0:
-                video = record[4][0] if video == None else video # what if no video for any record?
+            if len(record[3]) != 0:
+                video = record[3][0] if video == None else video # what if no video for any record?
 
             curr_score = score_time
 
@@ -144,7 +144,7 @@ def get_stage_total(stage_name, is_TAS):
 
     for character in BTT_CHARS_STAGE_COMMAND:
         cur = conn.cursor()
-        sql_q = f'SELECT * FROM btt_table WHERE character=\'{character}\' AND stage=\'{stage_name}\' AND tas={is_TAS} ORDER BY score ASC, date ASC;'
+        sql_q = f'SELECT player, score, sources, tags FROM btt_table WHERE character=\'{character}\' AND stage=\'{stage_name}\' AND tas={is_TAS} ORDER BY score ASC, date ASC;'
         cur.execute(sql_q)
         cur = filter_btt_tags([], cur)
 
@@ -155,17 +155,17 @@ def get_stage_total(stage_name, is_TAS):
         score_time = 0
         
         for record in cur:
-            if record[3] > curr_score:
+            if record[1] > curr_score:
                 break
-            elif record[3] == curr_score:
-                players.append(record[2])
+            elif record[1] == curr_score:
+                players.append(record[0])
                 continue
             char = record[0]
             stage = record[1]
-            players.append(record[2])
-            score_time = record[3]
-            if len(record[4]) != 0:
-                video = record[4][0] if video == None else video # what if no video for any record?
+            players.append(record[0])
+            score_time = record[1]
+            if len(record[3]) != 0:
+                video = record[3][0] if video == None else video # what if no video for any record?
 
             curr_score = score_time
 
